ddce7b1ed1509ad882808cdd01df9038654bbe49
victor6289295 <sativictor87@gmail.com>
2025-07-16 13:20:36 +0100

    first commit

---
+++ logical.py
@@ -0,0 +1,50 @@
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) + float(second_number)
+print(f"{first_number} + {second_number} = {sum:.2f}")
+
+print("****************")
+print("Subtraction")
+print("****************")
+print("Enter two numbers to sub")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) - float(second_number)
+print(f"{first_number} - {second_number} = {sum:.2f}")
+
+print("****************")
+print("Multiplication")
+print("****************")
+print("Enter two numbers to mul")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) * float(second_number)
+print(f"{first_number} * {second_number} = {sum:.2f}")
+
+print("****************")
+print("Division")
+print("****************")
+print("Enter two numbers to div")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {sum:.2f}")
+
+print("****************")
+print("Exponetial")
+print("****************")
+print("Enter two numbers to exp")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) ** float(second_number)
+print(f"{first_number} ** {second_number} = {sum:.2f}")
