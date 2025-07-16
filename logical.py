a57e5fac8e9b8b7612325ba71c1781661b2f949d
victor6289295 <sativictor87@gmail.com>
2025-07-16 14:24:02 +0100

    floor added

--- logical.py
+++ logical.py
@@ -48,3 +48,16 @@ first_number = input("First Number:")
 second_number = input("Second Number:")
 sum = float(first_number) ** float(second_number)
 print(f"{first_number} ** {second_number} = {sum:.2f}")
+
+print("****************")
+print("Floor")
+print("****************")
+print("Enter two numbers to floor")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+# prompt the user for number and store it
+second_number = input("Second Number:")
+sum = float(first_number) // float(second_number)
+print(f"{first_number} // {second_number} = {sum:.2f}")
+
+
